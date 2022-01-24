724057512ad0e0390abafc42f8647e4a1a35cff4
muhammadBilal924 <mbilal60924@gmail.com>
2022-01-24 11:50:20 +0500

    init Changes and components creation

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
+    <Home data="bilal khan"/>
+    <h2>Hello World2</h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello from home component</h1>
+    <h2>{{ data }}</h2>
+  </div>
+</template>
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    Name = "Home";
+  },
+});
+</script>
